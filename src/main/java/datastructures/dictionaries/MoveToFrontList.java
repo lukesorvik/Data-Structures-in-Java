86bed83c94218dfe714efc659169eb6230c38ba0
luke sorvik <lukesorv@uw.edu>
2024-02-02 19:45:46 -0800

    started shiii

--- src/main/java/datastructures/dictionaries/MoveToFrontList.java
+++ src/main/java/datastructures/dictionaries/MoveToFrontList.java
@@ -20,6 +20,27 @@ import java.util.Iterator;
  * dictionary/list's iterator.
  */
 public class MoveToFrontList<K, V> extends DeletelessDictionary<K, V> {
+    /*
+     * could use an array with front and end pointers
+     * since it is an array i can just swap the elements in place
+     *when using find can just iterate through the array and swap the elements
+     * sorted does not matter yippeeee
+     * add to the back of the array
+     * maybe use the data structure i implemented before (fifo queue??
+     * also look at the notes
+     * 
+     * 
+     * [1,2,0,0,0,0]
+     * F  B
+     * 
+     * not sure how to store multiple elements in the array?
+     * 
+     * 
+     * 
+     * 
+     */
+
+
     @Override
     public V insert(K key, V value) {
         throw new NotYetImplementedException();
